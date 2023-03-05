6bf7702f10b2b6e66c114510f2717f7373f8577b
ask03 <aykim043@gmail.com>
2023-03-05 02:09:46 -0500

    usdc balance

--- scripts/setup.js
+++ scripts/setup.js
@@ -8,12 +8,14 @@ const hre = require("hardhat");
 
 const fs = require('fs')
 const contractAddrs = fs.readFileSync('deployAddresses.txt').toString().split('\n').map(line => line.split('='));
-const wethAddr = contractAddrs.filter(pair => pair[0] == 'weth')[0][1]
+const wethAddr = contractAddrs.filter(pair => pair[0] == 'weth')[0][1];
+const usdcAddr = contractAddrs.filter(pair => pair[0] == 'usdc')[0][1];
     
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
   // let weth = await hre.ethers.getContractAt('WETH9', wethAddr /*, deployer*/)
   let weth = await (await hre.ethers.getContractFactory("WETH9")).attach(wethAddr);
+  let usdc = await (await hre.ethers.getContractFactory("USDC")).attach(usdcAddr);
   // console.log(weth)
   // let weth = await (await hre.ethers.getContractFactory("WETH9")).attach(wethAddr);
   await weth.deposit({ value: hre.ethers.utils.parseEther("10.0") });
@@ -21,6 +23,8 @@ async function main() {
   // let balance = await weth.balanceOf(deployer.address);
   // console.log("balance: ", balance);
   console.log("deployer WETH balance: ", hre.ethers.utils.formatEther(await weth.balanceOf(deployer.address)));
+  console.log("deployer USDC balance : ", hre.ethers.utils.formatUnits(await usdc.balanceOf(deployer.address), 6));
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
